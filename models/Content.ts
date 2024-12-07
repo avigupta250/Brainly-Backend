f6d883103f0dcef00bcade138cbe0f4fde5943d5
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-07 19:25:16 +0530

    type fixed

--- models/Content.ts
+++ models/Content.ts
@@ -8,7 +8,7 @@ const ContentSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ["image", "video", "article", "audio","youtube","twitter"],
+      enum: ["image", "video", "article", "music","youtube","twitter"],
       required: true,
     },
     description:{
